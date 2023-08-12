711669309ffec124b89bb21f94e9dff7fe5d452a
mitchpenrose <mitchpenrose@gmail.com>
2023-08-12 01:29:32 -0600

    added sort and search functionality

--- src/Components/WinRates.tsx
+++ src/Components/WinRates.tsx
@@ -3,7 +3,7 @@ import Context from "../Context/Context"
 import { JSX } from "react/jsx-runtime"
 import { getColorForPercentage } from "../Utils/utils"
 import styled from "styled-components"
-import Select from 'react-select'
+import Select, { ControlProps, GroupBase, components } from 'react-select'
 
 
 const StyledSelect = styled(Select)`
@@ -36,14 +36,21 @@ const StyledSelect = styled(Select)`
 `
 
 const positions = [{ value: 'ALL POSITIONS', label: 'ALL POSITIONS' }, { value: 'TOP', label: 'TOP' }, { value: 'JUNGLE', label: 'JUNGLE' }, { value: 'MID', label: 'MID' }, { value: 'ADC', label: 'ADC' }, { value: 'SUPPORT', label: 'SUPPORT' },]
+const sorting = [{ value: 'WIN RATE', label: 'WIN RATE' }, { value: 'PLAY RATE', label: 'PLAY RATE' }]
 interface Props {
     championWinRates: any
 }
 
 const WinRates = ({ championWinRates }: Props) => {
 
+    interface ChampStat { key: string, wins: number, losses: number, pct: number }
+    interface value {wins: number, losses: number}
+
     const data = useContext(Context)
     const [selectedPosition, setSelectedPosition] = useState(positions[0])
+    const [selectedSort, setSelectedSort] = useState(sorting[0])
+    const [currentChampStats, setCurrentChampStats] = useState<ChampStat[]>([])
+    const [filterValue, setFilterValue] = useState<string>('')
 
     const getColorForGames = (games: number) => {
         if(games < 500){
@@ -55,10 +62,7 @@ const WinRates = ({ championWinRates }: Props) => {
         return 'rgb(0,255,0)'
     }
 
-    interface ChampStat { key: string, wins: number, losses: number, pct: number }
-    interface value {wins: number, losses: number}
-
-    const overallWinRateData = useMemo(() => {
+    useMemo(() => {
         if (data.championToImage.size === 0 || selectedPosition.value !== 'ALL POSITIONS') {
             return
         }
@@ -77,35 +81,36 @@ const WinRates = ({ championWinRates }: Props) => {
             champStats.push({key: key, wins: value.wins, losses: value.losses, pct: value.wins / (value.wins + value.losses)})
         })
         champStats.sort((a, b) => { return b.pct - a.pct })
-        const length = champStats.length
-        //debugger
-        let row: JSX.Element[] = []
-        let rows: JSX.Element[][] = []
-        champStats.forEach((champStats: ChampStat, index: number) => {
-            row.push(<div style={{ marginLeft: '3px', position: 'relative' }} key={index}>
-                <img src={data.championToImage.get(champStats.key).src} />
-                <div style={{ position: "absolute", top: "0px", userSelect: "none", backgroundColor: "rgba(125, 125, 125, 0.5)"}}>{data.champData.find((data) => data.value === champStats.key)?.label}</div>
-                <div style={{ position: "absolute", bottom: "25px", color: getColorForPercentage(champStats.pct), userSelect: "none", backgroundColor: "black" }} key={index}>{(champStats.pct * 100).toFixed(2) + "%"}</div>
-                <div style={{ position: "absolute", bottom: "5px", color: getColorForGames(champStats.wins+champStats.losses), userSelect: "none", backgroundColor: "black" }} key={(index+1)*2}>{`${champStats.wins}/${champStats.wins+champStats.losses}`}</div>
-            </div>)
-            if (row.length === 9 || index === length - 1) {
-                rows.push(row)
-                row = []
-            }
-        })
-        return rows.map((r, index) => {
-            return <div key={index} style={{ display: 'flex' }}>{r}</div>
-        })
+        setCurrentChampStats(champStats)
     }, [data.championToImage, selectedPosition])
 
-    const winRateData = useMemo(() => {
+    useMemo(() => {
         if (data.championToImage.size === 0 || selectedPosition.value === 'ALL POSITIONS') {
             return
         }
+        setCurrentChampStats(championWinRates[selectedPosition.value])
+    }, [data.championToImage, selectedPosition])
+
+    const sortedFilteredData = useMemo(() => {
+        if(selectedSort.value === 'WIN RATE'){
+            currentChampStats.sort((a, b) => { return b.pct - a.pct })
+        }
+        else if(selectedSort.value === 'PLAY RATE'){
+            currentChampStats.sort((a, b) => { return (b.wins + b.losses) - (a.wins + a.losses) })
+        }
+        let filtered = currentChampStats
+        if(filterValue !== ''){
+            const filterOn = filterValue.toLowerCase()
+            filtered = currentChampStats.filter((ccs) => {
+                const legitName = data.champData.find((data) => data.value === ccs.key)?.label.toLowerCase()
+                return legitName?.includes(filterOn) || legitName?.replaceAll('\'', '').replaceAll(' ', '').replaceAll('.', '').includes(filterOn)
+            })
+        }
+
         let row: JSX.Element[] = []
         let rows: JSX.Element[][] = []
-        const length = championWinRates[selectedPosition.value].length
-        championWinRates[selectedPosition.value].forEach((champStats: ChampStat, index: number) => {
+        const length = filtered.length
+        filtered.forEach((champStats: ChampStat, index: number) => {
             row.push(<div style={{ marginLeft: '3px', position: 'relative' }} key={index}>
                 <img src={data.championToImage.get(champStats.key).src} />
                 <div style={{ position: "absolute", top: "0px", userSelect: "none", backgroundColor: "rgba(125, 125, 125, 0.5)"}}>{data.champData.find((data) => data.value === champStats.key)?.label}</div>
@@ -120,16 +125,37 @@ const WinRates = ({ championWinRates }: Props) => {
         return rows.map((r, index) => {
             return <div key={index} style={{ display: 'flex' }}>{r}</div>
         })
-
-    }, [data.championToImage, selectedPosition])
+    }, [currentChampStats, selectedSort, filterValue])
 
     return <div style={{ width: '1110px', margin: 'auto', padding: '50px' }}>
         <div style={{display: 'flex', marginBottom: '10px'}}>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '3px', marginRight: '10px', fontSize: 'large'}}>Position</div>
             <StyledSelect options={positions} onChange={(selected) => setSelectedPosition(selected as { value: string, label: string })} value={selectedPosition} />
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '20px', marginRight: '10px', fontSize: 'large'}}>Sort By</div>
+            <StyledSelect options={sorting} onChange={(selected) => {setSelectedSort(selected as { value: string, label: string })}} value={selectedSort} />
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '20px', marginRight: '10px', fontSize: 'large'}}>Search</div>
+            <StyledSelect
+                isSearchable={true}
+                isClearable={false}
+                components={{
+                DropdownIndicator: null,
+                IndicatorSeparator: null,
+                }}
+                onInputChange={(value, action) => {
+                    if(action.action === 'input-change'){
+                        setFilterValue(value)
+                    }
+                }}
+                onFocus={() => {
+                    setFilterValue('')
+                }}
+                onBlur={() => null}
+                options={[]}
+                menuIsOpen={false}
+                value={{value: '', label: filterValue}}
+            />
         </div>
-        {winRateData}
-        {overallWinRateData}
+        {sortedFilteredData}
     </div>
 }
 
