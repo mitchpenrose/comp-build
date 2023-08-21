92677dde710bbcd57c8d4d8d4f526d6685d3a567
mitchpenrose <mitchpenrose@gmail.com>
2023-08-21 15:57:26 -0600

    updated About section

--- src/App.tsx
+++ src/App.tsx
@@ -111,23 +111,52 @@ function App() {
   const setupInfoModal = (title: string) => {
     if (title === 'About') {
       setInfoModalContent(<div>
-        <div>Composition Builder is a dynamic tool created for League of Legends players to optimize team compositions, offering informed champion selections rooted in data from Platinum+ tier matches. It allows users to evaluate individual champions in different roles, comparing them against all other champions within their team or against opponents.</div>
-        <br />
-        <div>On the right or left side of the champion selection interface, users can find two tables: the Synergy table and the Matchup table. The Synergy table presents win rate data for the chosen champion in relation to their team members, with each row representing a different role: top, jungle, mid, ADC, and support. The 'Average Synergy' reflects the mean win rate based on all these combinations.</div>
-        <br />
-        <div>Conversely, the Matchup table analyzes how the selected champion fares against each opponent in their corresponding role, and the 'Average Matchup' offers a comprehensive view of the performance across all opponents.</div>
-        <br />
-        <div>Both these averages are then combined to form a 'Combined Results' value, providing an overview of the team's composition performance. However, it's important to remember that these statistics are not definitive indicators of match outcomes, but rather insightful references to comprehend historical champion synergy and matchups.</div>
-        <br />
-        <div>For users who are unsure of champion role efficiency, the '?' button is a tool that displays champions with historically high synergy and matchup win rates. These are ordered from highest to lowest, offering a ranking system for decision making.</div>
-        <br />
-        <div>Finally, users can clear a selected champion by clicking the button left or right of the champion dropdown. This allows for quick adjustments and recalculations, ensuring users can experiment with various team compositions with ease.</div>
-        <br />
-        <div>Composition Builder aims to enhance strategizing and planning, encouraging data-driven decisions to elevate gameplay.</div>
-      </div>)
+      <h2>Composition Builder</h2>
+      <p>
+        Composition Builder is a dynamic tool created for League of Legends players to optimize team compositions, offering informed champion selections rooted in data from Platinum+ tier matches. It allows users to evaluate individual champions in different roles, comparing them against all other champions within their team or against opponents.
+      </p>
+      <p>
+        On the right or left side of the champion selection interface, users can find two tables: the Synergy table and the Matchup table. The Synergy table presents win rate data for the chosen champion in relation to their team members, with each row representing a different role: top, jungle, mid, ADC, and support. The 'Average Synergy' reflects the mean win rate based on all these combinations.
+      </p>
+      <p>
+        Conversely, the Matchup table analyzes how the selected champion fares against each opponent in their corresponding role, and the 'Average Matchup' offers a comprehensive view of the performance across all opponents.
+      </p>
+      <p>
+        Both these averages are then combined to form a 'Combined Results' value, providing an overview of the team's composition performance. However, it's important to remember that these statistics are not definitive indicators of match outcomes, but rather insightful references to comprehend historical champion synergy and matchups.
+      </p>
+      <p>
+        For users who are unsure of champion role efficiency, the '?' button is a tool that displays champions with historically high synergy and matchup win rates. These are ordered from highest to lowest, offering a ranking system for decision making.
+      </p>
+      <p>
+        Finally, users can clear a selected champion by clicking the button left or right of the champion dropdown. This allows for quick adjustments and recalculations, ensuring users can experiment with various team compositions with ease.
+      </p>
+      <p>
+        Composition Builder aims to enhance strategizing and planning, encouraging data-driven decisions to elevate gameplay.
+      </p>
+
+      <h2>Win Rates</h2>
+      <p>
+        Win Rates offers a detailed breakdown of champion win rates for each role (top, jungle, mid, ADC, support) as well as an aggregated view of all roles. Users can tailor their view by sorting the data based on win rate or play rate, filtering by specific champion names, and specifying a minimum number of games for the results.
+      </p>
+      <p>
+        By clicking on a particular champion, a pop-up window will appear displaying how the selected champion performs against opponents in the chosen role. Within this window, users have the flexibility to modify the data parameters:
+      </p>
+      <ul>
+        <li>Champion: You can switch to another champion within the pop-up window.</li>
+        <li>Position: You can change the role for the selected champion, selecting from all roles, top, jungle, mid, ADC, or support.</li>
+        <li>Matchup Type: Choose whether you want to see how your selected champion synergizes with teammates (paired with) or competes against opponents (versus).</li>
+        <li>Opponent/Teammate Position: Choose the role of the champions your selected champion is being compared against.</li>
+      </ul>
+      <p>
+        The title of the pop-up window updates accordingly, displaying the format 'Champion in Position Versus/Paired with Champions in Position'. Clicking on a champion within the window will update the selected champion.
+      </p>
+      <p>
+        Additional customization options in the pop-up window include sorting by win rate or play rate, searching for specific champions, and setting a minimum number of games for the displayed results.
+      </p>
+    </div>)
     }
     if (title === 'Legal') {
-      setInfoModalContent(`Composition Builder isn't endorsed by Riot Games and doesn't reflect the views 
+      setInfoModalContent(`Compbuilder.gg isn't endorsed by Riot Games and doesn't reflect the views 
       or opinions of Riot Games or anyone officially involved in producing or managing 
       Riot Games properties. Riot Games, and all associated properties are trademarks 
       or registered trademarks of Riot Games, Inc.`)
@@ -141,7 +170,7 @@ function App() {
       setInfoModalContent(<div>
         <p>Last updated: 8/2/2023</p>
         <h2>1. Introduction</h2>
-        <p>Composition Builder values your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and use our dynamic tool for League of Legends players. Please read this Privacy Policy carefully. If you do not agree with the terms of this Privacy Policy, please do not access the site.</p>
+        <p>Compbuilder.gg values your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and use our dynamic tool for League of Legends players. Please read this Privacy Policy carefully. If you do not agree with the terms of this Privacy Policy, please do not access the site.</p>
         <h2>2. Information We Collect</h2>
         <p>We may collect information about you in various ways, including:</p>
         <ul>
@@ -153,8 +182,8 @@ function App() {
         <ul>
           <li>Enhance your user experience.</li>
           <li>Understand and analyze the usage trends and preferences of our users.</li>
-          <li>Improve the functionality and features of Composition Builder.</li>
-          <li>Monitor and analyze the effectiveness of Composition Builder and our marketing activities.</li>
+          <li>Improve the functionality and features of Compbuilder.gg.</li>
+          <li>Monitor and analyze the effectiveness of Compbuilder.gg and our marketing activities.</li>
           <li>Detect, prevent, or investigate security breaches or fraud.</li>
         </ul>
         <h2>4. How We Share Your Information</h2>
@@ -163,7 +192,7 @@ function App() {
         <p>We may use cookies, web beacons, and other tracking technologies to collect and store your information. We utilize Google Analytics to help us understand how our website is being used.</p>
         <p>If you wish to opt out of Google Analytics tracking, you may download and install the <a href="https://tools.google.com/dlpage/gaoptout">Google Analytics Opt-out Browser Add-on</a>. The add-on is designed to be compatible with Chrome, Safari, Firefox, and Microsoft Edge and allows website visitors the ability to prevent their data from being used by Google Analytics. More information about the opt-out and how to properly install the browser add-on can be found at the aforementioned link.</p>
         <h2>6. Security</h2>
-        <p>We take your security seriously. Composition Builder uses HTTPS and TLS encryption to ensure that the minimal information we collect is transmitted securely. We also employ reasonable administrative, technical, and physical security measures to protect your personal information. We strive to protect your information to the fullest extent possible.</p>
+        <p>We take your security seriously. Compbuilder.gg uses HTTPS and TLS encryption to ensure that the minimal information we collect is transmitted securely. We also employ reasonable administrative, technical, and physical security measures to protect your personal information. We strive to protect your information to the fullest extent possible.</p>
         <h2>7. Google AdSense</h2>
         <p>If we employ Google AdSense, we adhere to Google's policies, and we disclose the use of cookies and/or web beacons to collect data in the ad serving process.</p>
         <h2>8. Your Rights</h2>
